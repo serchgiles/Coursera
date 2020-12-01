5c02fa004fc8abd20871f928aad9fa8caab369be
serchgiles <60450256+serchgiles@users.noreply.github.com>
2020-12-01 11:05:25 -0500

    This is a commit from Visual Studio

--- Statistical Inference/project.R
+++ Statistical Inference/project.R
@@ -1,23 +1,19 @@
 rm(list = ls())
-
-
 library(tictoc)
 
 size <- 50
 simulations <- 1000
 meanVec <- numeric(simulations)
 sdVec <- numeric(simulations)
-tic()
 veces <- 2
+tic()
 for (i in 1:simulations) {
      simul <- rexp(size, 0.2)
      meanVec[i] <- mean(simul)
      sdVec[i] <- sd(simul)
 }
-
-f <- abs((meanVec-1/0.2))> veces*(1/(0.2*sqrt(size)))
-
 toc()
+f <- abs((meanVec-1/0.2))> veces*(1/(0.2*sqrt(size))) #Factor vector
 windows(title = "PLOT")
 par(mfrow = c(2,1))
 hist(meanVec, 31)
@@ -27,4 +23,4 @@ plot(meanVec, col = factor(f))
 abline(h = 1/0.2, lwd = 3 , col = 'blue')
 abline(h = 1/0.2 + veces*c(-1,1)/(0.2*sqrt(size)), lwd = 3,
        col = 'red')
-dev.off()
+#dev.off()
